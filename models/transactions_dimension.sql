cd5312032df60112b57af140a03bc92a048fea9c
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-02-19 13:53:00 +0200

    Update transactions_dimension.sql

--- models/transactions_dimension.sql
+++ models/transactions_dimension.sql
@@ -1,84 +1,81 @@
 -- models/transactions_dimension.sql
 
--- {{ config(materialized='table') }}
+{{ config(materialized='table') }}
 
--- SELECT
---     txndetailsid,
---     (createdat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS transaction_createdat_utc2,
---     (lastmodified::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS transaction_modifiedat_utc2,
---     txntype,
---     transactionstatus,
---     transactionchannel,
---     transactiondomain,
---     transactionaction,
---     interchangeaction,
+SELECT
+    txndetailsid,
+    (createdat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS transaction_createdat_utc2,
+    (lastmodified::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS transaction_modifiedat_utc2,
+    txntype,
+    transactionstatus,
+    transactionchannel,
+    transactiondomain,
+    transactionaction,
+    interchangeaction,
 
---     (now()::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') as loaddate,
---     null::timestamptz as expdate,
---     true::boolean as currentflag
+    (now()::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') as loaddate,
+    null::timestamptz as expdate,
+    true::boolean as currentflag
 
--- FROM {{ source('axis_core', 'transactiondetails')}}
+FROM {{ source('axis_core', 'transactiondetails')}}
 
--- models/transactions_dimension.sql
 
--- models/transactions_dimension.sql
+-- {{ config(
+--     materialized='incremental',
+--     unique_key='txndetailsid',
+--     strategy='timestamp',
+--     check_cols=['transaction_modifiedat_utc2']
+-- ) }}
 
-{{ config(
-    materialized='incremental',
-    unique_key='txndetailsid',
-    strategy='timestamp',
-    check_cols=['transaction_modifiedat_utc2']
-) }}
+-- WITH source_data as (
+--     SELECT
+--         txndetailsid,
+--         (createdat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS transaction_createdat_utc2,
+--         (lastmodified::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS transaction_modifiedat_utc2,
+--         txntype,
+--         transactionstatus,
+--         transactionchannel,
+--         transactiondomain,
+--         transactionaction,
+--         interchangeaction,
+--         (now()::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') as loaddate,
+--         null::timestamptz as expdate,
+--         true::boolean as currentflag
+--     FROM {{ source('axis_core', 'transactiondetails')}}
+-- )
 
-WITH source_data as (
-    SELECT
-        txndetailsid,
-        (createdat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS transaction_createdat_utc2,
-        (lastmodified::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS transaction_modifiedat_utc2,
-        txntype,
-        transactionstatus,
-        transactionchannel,
-        transactiondomain,
-        transactionaction,
-        interchangeaction,
-        (now()::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') as loaddate,
-        null::timestamptz as expdate,
-        true::boolean as currentflag
-    FROM {{ source('axis_core', 'transactiondetails')}}
-)
-
-MERGE INTO {{ this }} AS target
-USING source_data AS source
-ON target.txndetailsid = source.txndetailsid
-WHEN MATCHED AND target.transaction_modifiedat_utc2 < source.transaction_modifiedat_utc2 THEN
-    UPDATE SET
-        expdate = now()::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours',
-        currentflag = false
-WHEN NOT MATCHED THEN
-    INSERT (
-        txndetailsid,
-        transaction_createdat_utc2,
-        transaction_modifiedat_utc2,
-        txntype,
-        transactionstatus,
-        transactionchannel,
-        transactiondomain,
-        transactionaction,
-        interchangeaction,
-        loaddate,
-        expdate,
-        currentflag
-    ) VALUES (
-        source.txndetailsid,
-        source.transaction_createdat_utc2,
-        source.transaction_modifiedat_utc2,
-        source.txntype,
-        source.transactionstatus,
-        source.transactionchannel,
-        source.transactiondomain,
-        source.transactionaction,
-        source.interchangeaction,
-        source.loaddate,
-        source.expdate,
-        source.currentflag
-    );
+-- MERGE INTO {{ this }} AS target
+-- USING source_data AS source
+-- ON target.txndetailsid = source.txndetailsid
+-- WHEN MATCHED AND target.transaction_modifiedat_utc2 < source.transaction_modifiedat_utc2 THEN
+--     UPDATE SET
+--         expdate = now()::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours',
+--         currentflag = false
+-- WHEN NOT MATCHED THEN
+--     INSERT (
+--         txndetailsid,
+--         transaction_createdat_utc2,
+--         transaction_modifiedat_utc2,
+--         txntype,
+--         transactionstatus,
+--         transactionchannel,
+--         transactiondomain,
+--         transactionaction,
+--         interchangeaction,
+--         loaddate,
+--         expdate,
+--         currentflag
+--     ) VALUES (
+--         source.txndetailsid,
+--         source.transaction_createdat_utc2,
+--         source.transaction_modifiedat_utc2,
+--         source.txntype,
+--         source.transactionstatus,
+--         source.transactionchannel,
+--         source.transactiondomain,
+--         source.transactionaction,
+--         source.interchangeaction,
+--         source.loaddate,
+--         source.expdate,
+--         source.currentflag
+--     );
