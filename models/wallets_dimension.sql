1a595a8c30ada8cf7f9bd245b56f931d03e5bca6
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-02-28 15:56:31 +0200

    added timestamps to  wallets_dimension.sql

--- models/wallets_dimension.sql
+++ models/wallets_dimension.sql
@@ -5,8 +5,16 @@
 SELECT
     walletId,
     walletnumber,
+    
     (createdat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS wallet_createdat_utc2,
     (lastmodified::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS wallet_modifiedat_utc2,
+    (suspendedat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS wallet_suspendedat_utc2,
+    (unsuspendedat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS wallet_unsuspendedat_utc2,
+    (unregisteredat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS wallet_unregisteredat_utc2,
+    (activatedat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS wallet_activatedat_utc2,
+    (reactivatedat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS wallet_reactivatedat_utc2,
+    (lasttxnts::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS wallet_lasttxnts_utc2,
+    
     wallettype as wallet_type,
     walletStatus as wallet_status,
     
