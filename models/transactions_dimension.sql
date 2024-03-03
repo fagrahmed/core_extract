77c7915a0532a9c9aa817e996eccdb3c84b24045
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-03-03 14:46:32 +0200

    Update transactions_dimension.sql
    added consumer cost and revenue fields
    - hasservicefees
    - service_fees
    - interchange_amount
    - transactionstatus
    - amount

--- models/transactions_dimension.sql
+++ models/transactions_dimension.sql
@@ -14,6 +14,10 @@ SELECT
     transactiondomain,
     transactionaction,
     interchangeaction,
+    interchangeamount::float as interchange_amount,
+    servicefees_aibyte_transform::float as service_fees,
+    txnrequestedamount_aibyte_transform::float as amount,
+    hasservicefees
 
     (now()::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') as loaddate,
     null::timestamptz as expdate,
