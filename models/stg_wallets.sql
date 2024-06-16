5d7317162b182a869aacf04ca8a738c4515a4abd
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-06-16 22:02:19 +0300

    Update stg_wallets.sql

--- models/stg_wallets.sql
+++ models/stg_wallets.sql
@@ -15,13 +15,14 @@
 {% set stg_table_exists = stg_table_exists_result.rows[0][0] if stg_table_exists_result and stg_table_exists_result.rows else False %}
 
 
-{% set walletid_coalesced = COALESCE(walletid, '') %}
-{% set walletnumber_coalesced = COALESCE(walletnumber, '') %}
-{% set lastmodified_text = COALESCE(lastmodified | string, '') %}
-{% set now_text = now() | string %}
+{% set random_text = "random()::text" %}
+{% set walletid_coalesced = "COALESCE(walletid, '')" %}
+{% set walletnumber_coalesced = "COALESCE(walletnumber, '')" %}
+{% set lastmodified_text = "COALESCE(lastmodified::text, '')" %}
+{% set now_text = "now()::text" %}
 
 SELECT
-    {{ dbt_utils.generate_surrogate_key( walletid_coalesced, walletnumber_coalesced, lastmodified_text, now_text
+    {{ dbt_utils.generate_surrogate_key( random_text, walletid_coalesced, walletnumber_coalesced, lastmodified_text, now_text
     ) }} AS id,
     'insert' AS operation,
     'insert' AS operation,
