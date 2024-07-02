16b9fd5525bd21f995e4081cd6eb5e50829cad1d
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-07-02 14:04:04 +0300

    test

--- models/stg_wallets.sql
+++ models/stg_wallets.sql
@@ -12,45 +12,8 @@
 {% set stg_table_exists = stg_table_exists_result.rows[0][0] if stg_table_exists_result.rows else False %}
 
 SELECT
-    md5(
-        COALESCE(_airbyte_data->>'walletid', '') || '-' || 
-        COALESCE(_airbyte_data->>'walletnumber', '') || '-' || 
-        COALESCE(_airbyte_data->>'lastmodified', '') || '-' || 
-        (now()::timestamptz)::text
-    ) AS id,
     'insert' AS operation,
-    true AS currentflag,
-    null::timestamptz AS expdate,
-    COALESCE(_airbyte_data->>'walletid', null) AS walletid,
-    COALESCE(_airbyte_data->>'walletnumber', null) AS walletnumber,
-    md5(
-        COALESCE(_airbyte_data->>'walletid', '') || '::' || 
-        COALESCE(_airbyte_data->>'walletnumber', '') || '::' || 
-        COALESCE(_airbyte_data->>'walletStatus', '') || '::' || 
-        COALESCE(_airbyte_data->>'nationalid', '') || '::' || 
-        COALESCE(_airbyte_data->>'firstname', '') || '::' || 
-        COALESCE(_airbyte_data->>'lastname', '') || '::' || 
-        COALESCE(_airbyte_data->>'clientdata', '') || '::' || 
-        COALESCE(_airbyte_data->>'partnerid', '') || '::' || 
-        COALESCE(_airbyte_data->>'activatedat', '') || '::' || 
-        COALESCE(_airbyte_data->>'reactivatedat', '') || '::' || 
-        COALESCE((_airbyte_data->>'waivedamount')::text, '') || '::' || 
-        COALESCE(_airbyte_data->>'suspendedreason', '') || '::' || 
-        COALESCE((_airbyte_data->>'pinsetflag')::text, '')
-    ) AS hash_column,
-    ((_airbyte_data->>'createdat')::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS wallet_createdat_utc2,
-    ((_airbyte_data->>'lastmodified')::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS wallet_modifiedat_utc2,
-    (coalesce((_airbyte_data->>'suspendedat')::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours'), null) AS wallet_suspendedat_utc2,
-    (coalesce((_airbyte_data->>'unsuspendedat')::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours'), null) AS wallet_unsuspendedat_utc2,
-    (coalesce((_airbyte_data->>'unregisteredat')::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours'), null) AS wallet_unregisteredat_utc2,
-    (coalesce((_airbyte_data->>'activatedat')::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours'), null) AS wallet_activatedat_utc2,
-    (coalesce((_airbyte_data->>'reactivatedat')::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours'), null) AS wallet_reactivatedat_utc2,
-    (coalesce((_airbyte_data->>'lasttxnts')::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours'), null) AS wallet_lasttxnts_utc2,
-    COALESCE(_airbyte_data->>'wallettype', null) AS wallet_type,
-    COALESCE(_airbyte_data->>'walletStatus', null) AS wallet_status,
-    COALESCE(_airbyte_data->>'walletprofileid', null) AS profileid,
-    COALESCE(_airbyte_data->>'partnerid', null) AS partnerid,
-    (now()::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS loaddate
+    true AS currentflag
 FROM
     {{ source('axis_core', '_airbyte_raw_walletdetails') }} src
 {% if is_incremental() and table_exists and stg_table_exists %}
