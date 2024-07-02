8f062d57a53281a823dab2175d0730bc15687546
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-07-02 14:12:05 +0300

    Update stg_wallets.sql

--- models/stg_wallets.sql
+++ models/stg_wallets.sql
@@ -3,12 +3,7 @@
     on_schema_change='append_new_columns'
 )}}
 
-{% set table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'wallets_dimension')" %}
-{% set table_exists_result = run_query(table_exists_query) %}
-{% set table_exists = table_exists_result.rows[0][0] if table_exists_result and table_exists_result.rows else False %}
-{% set stg_table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'stg_wallets')" %}
-{% set stg_table_exists_result = run_query(stg_table_exists_query) %}
-{% set stg_table_exists = stg_table_exists_result.rows[0][0] if stg_table_exists_result.rows else False %}
+
 
 SELECT
     'insert' AS operation,
