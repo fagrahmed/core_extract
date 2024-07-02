da12bcbd2580046c1dca5d2e4623c2a01f39a996
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-07-02 12:49:52 +0300

    _airbyte_data->> for all columns in transactions_dimension.sql

--- models/transactions_dimension.sql
+++ models/transactions_dimension.sql
@@ -1,6 +1,3 @@
--- models/transactions_dimension.sql
-
-
 {{ config(materialized='incremental',
     unique_key= ['txndetailsid'],
     on_schema_change='fail') }}
@@ -9,40 +6,43 @@
 {% set table_exists_result = run_query(table_exists_query) %}
 {% set table_exists = table_exists_result.rows[0][0] if table_exists_result and table_exists_result.rows else False %}
 
-
 SELECT
-    md5(random()::text || '-' || COALESCE(txndetailsid, '') || '-' || COALESCE(walletdetailsid, '') || '-' || COALESCE(lastmodified::text, '') || '-' || now()::text) AS id,
+    md5(
+        random()::text || '-' || 
+        COALESCE(_airbyte_data->>'txndetailsid', '') || '-' || 
+        COALESCE(_airbyte_data->>'walletdetailsid', '') || '-' || 
+        COALESCE(_airbyte_data->>'lastmodified'::text, '') || '-' || 
+        now()::text
+    ) AS id,
     'insert' AS operation,
     true AS currentflag,
     null::timestamptz AS expdate,
-    txndetailsid,
-    walletdetailsid,
-    clientdetails,
-    (createdat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS transaction_createdat_utc2,
-    (lastmodified::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS transaction_modifiedat_utc2,
-    txntype,
-    transactionstatus,
-    transactionchannel,
-    transactiondomain,
-    transactionaction,
-    interchangeaction,
-    interchangeamount::float as interchange_amount,
-    servicefees_aibyte_transform::float as service_fees,
-    txnrequestedamount_aibyte_transform::float as amount,
-    walletbalancebefore_aibyte_transform::float as balance_before,
-    walletbalanceafter_aibyte_transform::float as balance_after,
-    walletactualbalancebefore_aibyte_transform as actual_balance_before,
-    walletactualbalanceafter_aibyte_transform as actual_balance_after,
-    hasservicefees,
-    transactionreference,
-    isreversedflag,
+    _airbyte_data->>'txndetailsid' AS txndetailsid,
+    _airbyte_data->>'walletdetailsid' AS walletdetailsid,
+    _airbyte_data->>'clientdetails' AS clientdetails,
+    (_airbyte_data->>'createdat'::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS transaction_createdat_utc2,
+    (_airbyte_data->>'lastmodified'::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS transaction_modifiedat_utc2,
+    _airbyte_data->>'txntype' AS txntype,
+    _airbyte_data->>'transactionstatus' AS transactionstatus,
+    _airbyte_data->>'transactionchannel' AS transactionchannel,
+    _airbyte_data->>'transactiondomain' AS transactiondomain,
+    _airbyte_data->>'transactionaction' AS transactionaction,
+    _airbyte_data->>'interchangeaction' AS interchangeaction,
+    (_airbyte_data->>'interchangeamount')::float AS interchange_amount,
+    (_airbyte_data->>'servicefees_aibyte_transform')::float AS service_fees,
+    (_airbyte_data->>'txnrequestedamount_aibyte_transform')::float AS amount,
+    (_airbyte_data->>'walletbalancebefore_aibyte_transform')::float AS balance_before,
+    (_airbyte_data->>'walletbalanceafter_aibyte_transform')::float AS balance_after,
+    _airbyte_data->>'walletactualbalancebefore_aibyte_transform' AS actual_balance_before,
+    _airbyte_data->>'walletactualbalanceafter_aibyte_transform' AS actual_balance_after,
+    _airbyte_data->>'hasservicefees' AS hasservicefees,
+    _airbyte_data->>'transactionreference' AS transactionreference,
+    _airbyte_data->>'isreversedflag' AS isreversedflag,
     (now()::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS loaddate,
     CASE
-        WHEN txntype NOT LIKE '%FEES' OR txntype = 'TransactionTypes_CREATE-VCN_FEES' THEN false
+        WHEN _airbyte_data->>'txntype' NOT LIKE '%FEES' OR _airbyte_data->>'txntype' = 'TransactionTypes_CREATE-VCN_FEES' THEN false
         ELSE true
     END AS is_fees
-    
-
 
 FROM {{ source('axis_core', '_airbyte_raw_transactiondetails')}} src
 
