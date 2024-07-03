8b9e96d64531777ecc6d668d6647bf12c91d64bc
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-07-03 17:12:31 +0300

    Update and rename wallets_dimension.sql to inc_wallets_dimension.sql
    changed all sources/destinations to inc_(name)

--- models/inc_wallets_dimension.sql
+++ models/inc_wallets_dimension.sql
@@ -6,22 +6,22 @@
         unique_key= "hash_column",
         on_schema_change='fail',
         post_hook="
-            DROP TABLE IF EXISTS stg_wallets;
-            DROP TABLE IF EXISTS stg_wallets_2;
+            DROP TABLE IF EXISTS inc_stg_wallets;
+            DROP TABLE IF EXISTS inc_stg_wallets_2;
             "
     )
 }}
 
 with step_1 as (
     select stg.*
-    from {{ ref("stg_wallets") }} stg 
-    left join {{ ref("stg_wallets_2")}} stg2 on stg.hash_column = stg2.hash_column
+    from {{ ref("inc_stg_wallets") }} stg 
+    left join {{ ref("inc_stg_wallets_2")}} stg2 on stg.hash_column = stg2.hash_column
     where stg2.hash_column is null
 
     union 
 
     select *
-    from {{ref("stg_wallets_2")}}
+    from {{ref("inc_stg_wallets_2")}}
 )
 
 {%if is_incremental() %}
