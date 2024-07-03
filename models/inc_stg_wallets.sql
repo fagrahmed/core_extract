37ce0da4dcde5d4e150cdbef6b08a86725d7203b
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-07-03 17:10:02 +0300

    Update and rename stg_wallets.sql to inc_stg_wallets.sql
    changed all sources names to inc_(name)

--- models/inc_stg_wallets.sql
+++ models/inc_stg_wallets.sql
@@ -4,11 +4,11 @@
     on_schema_change='append_new_columns'
 )}}
     
-{% set table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'wallets_dimension')" %}
+{% set table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'inc_wallets_dimension')" %}
 {% set table_exists_result = run_query(table_exists_query) %}
 {% set table_exists = table_exists_result.rows[0][0] if table_exists_result and table_exists_result.rows else False %}
     
-{% set stg_table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'stg_wallets')" %}
+{% set stg_table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'inc_stg_wallets')" %}
 {% set stg_table_exists_result = run_query(stg_table_exists_query) %}
 {% set stg_table_exists = stg_table_exists_result.rows[0][0] if stg_table_exists_result and stg_table_exists_result.rows else False %}
 SELECT
@@ -42,5 +42,5 @@ SELECT
 FROM
     {{ source('axis_core', 'walletdetails') }} src
 {% if is_incremental() and table_exists and stg_table_exists %}
-    WHERE src._airbyte_emitted_at > COALESCE((SELECT max(loaddate::timestamptz) FROM {{ source('dbt-dimensions', 'wallets_dimension') }}), '1900-01-01'::timestamp)
+    WHERE src._airbyte_emitted_at > COALESCE((SELECT max(loaddate::timestamptz) FROM {{ source('dbt-dimensions', 'inc_wallets_dimension') }}), '1900-01-01'::timestamp)
 {% endif %}
