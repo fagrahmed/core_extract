c620c42354b901d2ffa1245c7d464c379eef3585
frefaat <frefaat@axispay.app>
2024-07-08 23:28:56 +0300

    source name correction

--- models/inc_wallets_stg_exp.sql
+++ models/inc_wallets_stg_exp.sql
@@ -5,7 +5,7 @@
     depends_on=['inc_wallets_stg'],
     on_schema_change='append_new_columns',
     pre_hook=[
-        "{% if target.schema == 'dbt-dimensions' and source('dbt-dimensions', 'inc_stg_wallets_exp') is not none %}TRUNCATE TABLE {{ source('dbt-dimensions', 'inc_stg_wallets_exp') }};{% endif %}"
+        "{% if target.schema == 'dbt-dimensions' and source('dbt-dimensions', 'inc_wallets_stg_exp') is not none %}TRUNCATE TABLE {{ source('dbt-dimensions', 'inc_stg_wallets_exp') }};{% endif %}"
     ]
 )}}
 
