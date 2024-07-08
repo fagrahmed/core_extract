86765f8be8b153206b59ab1acce2ebb0c2e5864e
frefaat <frefaat@axispay.app>
2024-07-08 23:40:44 +0300

    removed pre-hook

--- models/inc_wallets_stg.sql
+++ models/inc_wallets_stg.sql
@@ -1,10 +1,7 @@
 {{ config(
     materialized='incremental',
     unique_key= ['walletid', 'walletnumber'],
-    on_schema_change='append_new_columns',
-    pre_hook=[
-        "{% if target.schema == 'dbt-dimensions' and source('dbt-dimensions', 'inc_wallets_stg') is not none %}TRUNCATE TABLE {{ source('dbt-dimensions', 'inc_wallets_stg') }};{% endif %}"
-    ]
+    on_schema_change='append_new_columns'
 )}}
     
 {% set table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'inc_wallets_dimension')" %}
