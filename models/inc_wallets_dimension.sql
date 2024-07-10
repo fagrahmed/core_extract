8a6e28f17801402dfe40baa2fd1b9f0d3b2be921
frefaat <frefaat@axispay.app>
2024-07-10 16:39:20 +0300

    set _ for dependency referencs

--- models/inc_wallets_dimension.sql
+++ models/inc_wallets_dimension.sql
@@ -4,15 +4,20 @@
     config(
         materialized="incremental",
         unique_key= "hash_column",
-        on_schema_change='append_new_columns',
-		depends_on=[ref('inc_wallets_stg_no_change'), ref('inc_wallets_stg_update'), ref('inc_wallets_stg_exp'), ref('inc_wallets_stg_new'), ref('inc_wallets_stg')]
-    )
+        on_schema_change='append_new_columns'
+	)
 }}
 
 {% set table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'inc_wallets_dimension')" %}
 {% set table_exists_result = run_query(table_exists_query) %}
 {% set table_exists = table_exists_result.rows[0][0] if table_exists_result and table_exists_result.rows else False %}
 
+-- Ensure dependencies are clearly defined for dbt
+{% set _ = ref('inc_wallets_stg_update') %}
+{% set _ = ref('inc_wallets_stg_exp') %}
+{% set _ = ref('inc_wallets_stg_new') %}
+{% set _ = ref('inc_wallets_stg_no_change') %}
+{% set _ = ref('inc_wallets_stg') %}
 
 {% if table_exists %}
 
--- models/inc_wallets_stg_no_change.sql
+++ models/inc_wallets_stg_no_change.sql
@@ -4,14 +4,17 @@
     config(
         materialized="incremental",
         unique_key= "hash_column",
-        on_schema_change='append_new_columns',
-        depends_on=[ref('inc_wallets_stg_update'), ref('inc_wallets_stg_exp')]
+        on_schema_change='append_new_columns'
     )
 }}
 
 {% set table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'inc_wallets_dimension')" %}
 {% set table_exists_result = run_query(table_exists_query) %}
 {% set table_exists = table_exists_result.rows[0][0] if table_exists_result and table_exists_result.rows else False %}
+
+-- Ensure dependencies are clearly defined for dbt
+{% set _ = ref('inc_wallets_stg_update') %}
+{% set _ = ref('inc_wallets_stg_exp') %}
 
 {% if table_exists %}
 
