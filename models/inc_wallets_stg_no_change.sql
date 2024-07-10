a42d26861db517ac50863d53f6814d053e817d8d
frefaat <frefaat@axispay.app>
2024-07-10 16:13:12 +0300

    depends on in no_change

--- models/inc_wallets_stg_no_change.sql
+++ models/inc_wallets_stg_no_change.sql
@@ -4,7 +4,8 @@
     config(
         materialized="incremental",
         unique_key= "hash_column",
-        on_schema_change='append_new_columns'
+        on_schema_change='append_new_columns',
+        depends_on=['inc_wallets_stg_update', 'inc_wallets_stg_exp']
     )
 }}
 
