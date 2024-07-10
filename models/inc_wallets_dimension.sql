2a355410a5b92346aa7cf34142382900b2dab225
frefaat <frefaat@axispay.app>
2024-07-10 13:14:16 +0300

    final_records cte

--- models/inc_wallets_dimension.sql
+++ models/inc_wallets_dimension.sql
@@ -13,83 +13,86 @@
 {% set table_exists_result = run_query(table_exists_query) %}
 {% set table_exists = table_exists_result.rows[0][0] if table_exists_result and table_exists_result.rows else False %}
 
+WITH final_records AS (
+	SELECT
+		id,
+		operation,
+		currentflag,
+		expdate,
+		walletid,
+		walletnumber,
+		hash_column,
+		wallet_createdat_local,
+		wallet_modifiedat_local,
+		wallet_suspendedat_local,
+		wallet_unsuspendedat_local,
+		wallet_unregisteredat_local,
+		wallet_activatedat_local,
+		wallet_reactivatedat_local,
+		wallet_lasttxnts_local,
+		utc,
+		wallet_type,
+		wallet_status,
+		profileid,
+		partnerid,
+		loaddate
 
-SELECT
-	id,
-	operation,
-	currentflag,
-	expdate,
-	walletid,
-	walletnumber,
-	hash_column,
-	wallet_createdat_local,
-	wallet_modifiedat_local,
-	wallet_suspendedat_local,
-	wallet_unsuspendedat_local,
-	wallet_unregisteredat_local,
-	wallet_activatedat_local,
-	wallet_reactivatedat_local,
-	wallet_lasttxnts_local,
-	utc,
-	wallet_type,
-	wallet_status,
-	profileid,
-	partnerid,
-	loaddate
+	FROM {{ref("inc_wallets_stg_update")}}
 
-FROM {{ref("inc_wallets_stg_update")}}
+	UNION ALL
 
-UNION ALL
+	SELECT 
+		id,
+		operation,
+		currentflag,
+		expdate,
+		walletid,
+		walletnumber,
+		hash_column,
+		wallet_createdat_local,
+		wallet_modifiedat_local,
+		wallet_suspendedat_local,
+		wallet_unsuspendedat_local,
+		wallet_unregisteredat_local,
+		wallet_activatedat_local,
+		wallet_reactivatedat_local,
+		wallet_lasttxnts_local,
+		utc,
+		wallet_type,
+		wallet_status,
+		profileid,
+		partnerid,
+		loaddate
 
-SELECT 
-	id,
-	operation,
-	currentflag,
-	expdate,
-	walletid,
-	walletnumber,
-	hash_column,
-	wallet_createdat_local,
-	wallet_modifiedat_local,
-	wallet_suspendedat_local,
-	wallet_unsuspendedat_local,
-	wallet_unregisteredat_local,
-	wallet_activatedat_local,
-	wallet_reactivatedat_local,
-	wallet_lasttxnts_local,
-	utc,
-	wallet_type,
-	wallet_status,
-	profileid,
-	partnerid,
-	loaddate
-
-FROM {{ref("inc_wallets_stg_exp")}}
+	FROM {{ref("inc_wallets_stg_exp")}}
 
 
-UNION ALL
+	UNION ALL
 
-SELECT
-	id,
-	operation,
-	currentflag,
-	expdate,
-	walletid,
-	walletnumber,
-	hash_column,
-	wallet_createdat_local,
-	wallet_modifiedat_local,
-	wallet_suspendedat_local,
-	wallet_unsuspendedat_local,
-	wallet_unregisteredat_local,
-	wallet_activatedat_local,
-	wallet_reactivatedat_local,
-	wallet_lasttxnts_local,
-	utc,
-	wallet_type,
-	wallet_status,
-	profileid,
-	partnerid,
-	loaddate
+	SELECT
+		id,
+		operation,
+		currentflag,
+		expdate,
+		walletid,
+		walletnumber,
+		hash_column,
+		wallet_createdat_local,
+		wallet_modifiedat_local,
+		wallet_suspendedat_local,
+		wallet_unsuspendedat_local,
+		wallet_unregisteredat_local,
+		wallet_activatedat_local,
+		wallet_reactivatedat_local,
+		wallet_lasttxnts_local,
+		utc,
+		wallet_type,
+		wallet_status,
+		profileid,
+		partnerid,
+		loaddate
 
-FROM {{ref("inc_wallets_stg_new")}}
+	FROM {{ref("inc_wallets_stg_new")}}
+)
+
+SELECT * FROM final_records
